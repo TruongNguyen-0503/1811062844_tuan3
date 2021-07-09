8cb1693c0bbb6bf2083e748c4e527c44aa0afe64
AnhTruong-0503 <nguyencaoanhtruong53@gmail.com>
2021-07-09 10:33:03 +0700

    chinh sua ngay 9/7

--- 1811062844_Nguyencaoanhtruong_Tuan3/Controllers/HomeController.cs
+++ 1811062844_Nguyencaoanhtruong_Tuan3/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using _1811062844_Nguyencaoanhtruong_Tuan3.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace _1811062844_Nguyencaoanhtruong_Tuan3.Controllers
 {
@@ -10,7 +12,12 @@ namespace _1811062844_Nguyencaoanhtruong_Tuan3.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var upcomingCourses = _dbContext.Courses
+                .Include(c => c.Lecturer)
+                .Include(c => c.Category)
+                .Where(c => c.DateTime > DateTime.Now);
+            return View(upcomingCourses);
+
         }
 
         public ActionResult About()
@@ -26,5 +33,11 @@ namespace _1811062844_Nguyencaoanhtruong_Tuan3.Controllers
 
             return View();
         }
+        private ApplicationDbContext _dbContext;
+        public HomeController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+       
     }
 }
